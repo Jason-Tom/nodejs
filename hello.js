7c0dcb8656149ff35331d0512f9fb21d3dc78944
Jason-Tom <429295712@qq.com>
2016-08-31 17:36:18 +0800

    Revert "updata"
    This reverts commit 0d3b127b6b027ed5cb7a4477b21425e0a83c4468.

--- hello.js
+++ hello.js
@@ -8,5 +8,11 @@
 
 // 统一用一个接口来装载比较好管理，语句也能放在方法里面，算是接口的一个规范吧
 exports.world = function Hello() {
-    console.log('Hello ');
+    var name;
+    this.setName = function(thyName) {
+        name = thyName;
+    };
+    this.sayHello = function() {
+        console.log('Hello ' + name);
+    };
 };
--- 创建模块.js
+++ 创建模块.js
@@ -9,11 +9,12 @@
 // 
 // var hello = require('./hello');
 // hello.world();
-var hahaha = require('./hello');
+var world = require('./hello是1');
 // 自己写的模块引入不要去掉根路径
 // 模块引入文件能包含中文名，但是最好不要有中文
 // world = new world();
 // world.setName('BYVoid');
 // world.sayHello();
 // 间接创建一个同样的新对象来执行
-hahaha.world();
+var lalala = new a;
+lalala();
